60b5ab5a06e7c3a26a689da718eaa2e8b4dad6ce
Muhammad Farabie <farabie024@gmail.com>
2023-10-21 14:03:15 +0700

    Firebase Storage + Image Picker

--- lib/database_services.dart
+++ lib/database_services.dart
@@ -1,4 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:firebase_storage/firebase_storage.dart';
 
 class DatabaseServices {
   //Pointer atau collection untuk menujukkan ke collection di cloud firestore
@@ -21,5 +24,28 @@ class DatabaseServices {
 
   static Future<void> deleteProduct(String id) async {
     return await productCollection.doc(id).delete();
+  }
+
+  static Future<String> uploadImage(File imageFile) async {
+    //Fungsi untuk mengambil nama file yang akan diupload
+    String fileName = path.basename(imageFile.path);
+
+    //Buat reference untuk memasukannya ke firebase storage
+    Reference ref = FirebaseStorage.instance.ref().child(fileName);
+    //Buat fungsi untuk uploadnya
+    UploadTask task = ref.putFile(imageFile);
+    //Kondisi untuk jika dia selesai diupload
+    TaskSnapshot snapshot = await task.whenComplete(() async {
+      if (task.snapshot.state == TaskState.success) {
+        //Upload Sucessfull
+        print("Upload Complete");
+        String downloadURL = await ref.getDownloadURL();
+        return downloadURL;
+      } else {
+        throw Exception("Image Upload Failed");
+      }
+    });
+
+    return await snapshot.ref.getDownloadURL();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_anonymousflut/auth_services.dart';
 import 'package:firebase_anonymousflut/database_services.dart';
 import 'package:firebase_anonymousflut/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:path/path.dart' as path;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,11 +16,88 @@ void main() async {
   runApp(const FirebaseAnonym());
 }
 
-class FirebaseAnonym extends StatelessWidget {
+class FirebaseAnonym extends StatefulWidget {
   const FirebaseAnonym({super.key});
 
   @override
+  State<FirebaseAnonym> createState() => _FirebaseAnonymState();
+}
+
+class _FirebaseAnonymState extends State<FirebaseAnonym> {
+  String? imagePath;
+  @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Firebase Storage Demo"),
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                (imagePath != null)
+                    ? Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.black),
+                          image: DecorationImage(
+                            image: NetworkImage(imagePath!),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                    : Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.black),
+                        ),
+                      ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                  child: Text("Sign In"),
+                  onPressed: () async {
+                    AuthServices.signInWithAnonymous();
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("Upload Image"),
+                  onPressed: () async {
+                    File? file = await getImage();
+                    imagePath = await DatabaseServices.uploadImage(file!);
+
+                    setState(() {});
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Future<File?> getImage() async {
+  XFile? xFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+  if (XFile != null) {
+    //Convert XFile to File
+    File file = File(xFile!.path);
+    return file;
+  }
+  return null;
+}
+
+
     // return StreamProvider.value(
     //   initialData: null,
     //   value: AuthServices.userStream,
@@ -26,64 +107,62 @@ class FirebaseAnonym extends StatelessWidget {
     //   ),
     // );
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Cloud Firstore Demo"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
-                  DatabaseServices.createOrUpdateProduct(
-                    "2",
-                    name: "Pens",
-                    price: 10000,
-                  );
-                },
-                child: Text(
-                  "Add Data",
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  DatabaseServices.createOrUpdateProduct(
-                    "1",
-                    name: "Pens",
-                    price: 200000,
-                  );
-                },
-                child: Text(
-                  "Edit Data",
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  DocumentSnapshot snapshot =
-                      await DatabaseServices.getProduct("1");
-                  //snapshot.data['nama'] itu diganti jadi => snapshot.get('nama');
-                  print(snapshot.get('nama'));
-                  print(snapshot.get('harga'));
-                },
-                child: Text(
-                  "Get Data",
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  DatabaseServices.deleteProduct("1");
-                },
-                child: Text(
-                  "Delete Data",
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+    // return MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text("Cloud Firstore Demo"),
+    //     ),
+    //     body: Center(
+    //       child: Column(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //         children: <Widget>[
+    //           ElevatedButton(
+    //             onPressed: () {
+    //               DatabaseServices.createOrUpdateProduct(
+    //                 "2",
+    //                 name: "Pens",
+    //                 price: 10000,
+    //               );
+    //             },
+    //             child: Text(
+    //               "Add Data",
+    //             ),
+    //           ),
+    //           ElevatedButton(
+    //             onPressed: () {
+    //               DatabaseServices.createOrUpdateProduct(
+    //                 "1",
+    //                 name: "Pens",
+    //                 price: 200000,
+    //               );
+    //             },
+    //             child: Text(
+    //               "Edit Data",
+    //             ),
+    //           ),
+    //           ElevatedButton(
+    //             onPressed: () async {
+    //               DocumentSnapshot snapshot =
+    //                   await DatabaseServices.getProduct("1");
+    //               //snapshot.data['nama'] itu diganti jadi => snapshot.get('nama');
+    //               print(snapshot.get('nama'));
+    //               print(snapshot.get('harga'));
+    //             },
+    //             child: Text(
+    //               "Get Data",
+    //             ),
+    //           ),
+    //           ElevatedButton(
+    //             onPressed: () {
+    //               DatabaseServices.deleteProduct("1");
+    //             },
+    //             child: Text(
+    //               "Delete Data",
+    //             ),
+    //           )
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
